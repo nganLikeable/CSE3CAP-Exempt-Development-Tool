18fd48730382d30c996a93ab0eb2a18af6ed7eb1
Devjoy-chakma <165554234+Devjoy-chakma@users.noreply.github.com>
2025-09-14 18:14:34 +1000

    property 002

--- properties.js
+++ properties.js
@@ -22,5 +22,22 @@ window.PROPERTIES = [
       setbackFront_m: 7, setbackSide_m: 0.9, setbackRear_m: 1.0
     }
   },
-  
+  {
+    id: "002",
+    address: "5 Rivergum Ct, Thurgoona",
+    zone: "R2",
+    lotSize_m2: 1200,
+    bushfireProne: true,
+    heritageListed: false,
+    cornerLot: false,
+    floodAffected: false,
+    slopeRise_m: 0.3,
+    proposal: {
+      type: "verandah", // “Patio 4x5m attached to rear” ≈ verandah/pergola
+      width_m: 4, length_m: 5, height_m: 2.7,
+      attachedToDwelling: true,
+      location: "rear",
+      setbackFront_m: 8, setbackSide_m: 0.9, setbackRear_m: 1.5
+    }
+  },
 ];
