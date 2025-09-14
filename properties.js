f70eb360a4be874a0f9a0584f6310c0558dd492c
Devjoy-chakma <165554234+Devjoy-chakma@users.noreply.github.com>
2025-09-14 18:10:30 +1000

    Property 001

---
+++ properties.js
@@ -0,0 +1,26 @@
+
+// 15 sample properties.
+
+
+window.PROPERTIES = [
+ 
+  {
+    id: "001",
+    address: "12 Wattle Street, Albury",
+    zone: "R1",
+    lotSize_m2: 700,
+    bushfireProne: false,
+    heritageListed: false,
+    cornerLot: false,
+    floodAffected: false,
+    slopeRise_m: 0.5,
+    proposal: {
+      type: "shed",
+      width_m: 3, length_m: 4, height_m: 2.4,
+      attachedToDwelling: false,
+      location: "rear",
+      setbackFront_m: 7, setbackSide_m: 0.9, setbackRear_m: 1.0
+    }
+  },
+  
+];
